d975026bb8d16f4e44df34deb1d1b6669ba53328
dingding <johndoe@doebrothers.com>
2020-05-25 18:00:13 +0800

    Initial commit

---
+++ coupon/src/test/java/com/zaqizaba/coupon/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaqizaba.coupon.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ product/src/test/java/comg/zaqizaba/product/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package comg.zaqizaba.product.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
